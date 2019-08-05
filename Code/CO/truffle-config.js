fd68e3626af5b9e2c0b09a579ff2f625600a5881
Natasha <natashaoates1@gmail.com>
2019-08-05 09:49:27 +0200

    Code Config

--- Code/CO/truffle-config.js
+++ Code/CO/truffle-config.js
@@ -5,12 +5,12 @@ module.exports = {
   // see <http://truffleframework.com/docs/advanced/configuration>
   // for more details on how to specify configuration options!
   //
-  //networks: {
-  //  development: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  },
+networks: {
+  development: {
+    host: "127.0.0.1",     
+    port: 7545,            
+    network_id: "*",       
+   },
   //  test: {
   //    host: "127.0.0.1",
   //    port: 7545,
@@ -18,4 +18,26 @@ module.exports = {
   //  }
   //}
   //
+
+},
+
+// Set default mocha options here, use special reporters etc.
+mocha: {
+  // timeout: 100000
+},
+
+// Configure your compilers
+compilers: {
+  solc: {
+    // version: "0.5.1",    
+    // docker: true,        
+    // settings: {         
+    //  optimizer: {
+    //    enabled: false,
+    //    runs: 200
+    //  },
+    //  evmVersion: "byzantium"
+    // }
+  }
+}
 };
